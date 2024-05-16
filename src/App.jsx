8b039e624cce2153bd899dfab3e443914b53e508
Miguel T <mgltorsa@gmail.com>
2024-05-15 21:27:49 -0400

    apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,68 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Luz from "./containers/Luz";
-import Formulario from "./components/Formulario";
-import UserContext from "./contexts/UserContext";
-import CounterState from "./containers/CounterState";
-import CounterReducer from "./containers/CounterReducer";
-import CounterHook from "./containers/CounterHook";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+const fetchUsage = (setCountries, setError) => {
+  const promise = fetch("https://restcountries.com/v3.1/all", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  promise
+    .then((result) => {
+      if (result.ok) {
+        return result.json();
+      } else {
+        throw new Error("Was not ok");
+      }
+    })
+    .then((responseJson) => {
+      //HTTP 200
+      const countriesJson = responseJson;
+      console.log("countries", countriesJson);
+      setCountries(countriesJson);
+    })
+    .catch((error) => {
+      console.error(error);
+      setError(error);
+    });
+};
+
+const fetchAxiosCountries = async (setCountries, setError) => {
+  const response = await axios.get("https://restcountries.com/v3.1/all", {
+    headers: {
+      Authorization: "Bearer token123",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const countriesData = response.data;
+  setCountries(countriesData);
+};
 
 function App() {
-  const user = {
-    username: "miusername",
-  };
+  const [countries, setCountries] = useState([]);
+
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetchAxiosCountries(setCountries, setError);
+  }, []);
+
   return (
     <>
-      {/* <Luz /> */}
-      <UserContext.Provider value={user}>
-        <Formulario />
-        <CounterState />
-        <CounterReducer />
-        <CounterHook />
-      </UserContext.Provider>
+      <h1>Hola</h1>
+      <ul>
+        {countries.map((country, index) => {
+          return (
+            <li key={index.toString()}>
+              <h3>{country.name.common}</h3>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
